e800d64138cbd09fec48580f465b0104ea70e9dc
Ken <ken.lapolla@gmail.com>
2019-05-05 09:43:29 -0400

    Lesson 1 Completed

---
+++ Lesson_1/class_examples/common_datatypes.js
@@ -0,0 +1,18 @@
+//Strings
+var basic_string = "Hello World!";
+console.log(basic_string);
+console.log(typeof(basic_string));
+
+var basic_number = 7
+console.log(basic_number);
+console.log(typeof(basic_number));
+
+
+var advanced_number = 7.14
+console.log(advanced_number);
+console.log(typeof(advanced_number));
+
+
+var basic_boolean = true
+console.log(basic_boolean);
+console.log(typeof(basic_boolean));
